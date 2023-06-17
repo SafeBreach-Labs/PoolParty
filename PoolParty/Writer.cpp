fa5813b179add3cbc8cd74abed84ce53ab2270b0
Alon Leviev <alon.leviev@safebreach.com>
2023-06-17 15:23:38 +0300

    Removed unused writer.cpp

--- PoolParty/Writer.cpp
+++
@@ -1,7 +0,0 @@
-#include "Writer.h"
-
-void WriteMemory(HANDLE hTargetPid, LPVOID AllocatedMemory, char* cBuffer, SIZE_T szSizeOfShellcode) {
-	if (!WriteProcessMemory(hTargetPid, AllocatedMemory, cBuffer, szSizeOfShellcode, NULL)) {
-		throw WindowsException("WriteProcessMemory");
-	}
-}
